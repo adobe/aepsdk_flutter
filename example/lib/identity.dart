29df8acfcfabdd3c4baa0a0becb08656ef5d3147
Calise Cheung <cacheung@adobe.com>
2022-07-15 19:26:40 -0700

    Update Sample App  and Add license
    Update Sample App  and Add license

--- example/lib/identity.dart
+++ example/lib/identity.dart
@@ -1,9 +1,20 @@
-import 'dart:developer';
+/*
+Copyright 2022 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
 
+import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_aepcore/flutter_aepcore_data.dart';
 import 'package:flutter_aepcore/flutter_aepidentity.dart';
+import 'util.dart';
 
 class IdentityPage extends StatefulWidget {
   @override
@@ -129,25 +140,6 @@ class _MyAppState extends State<IdentityPage> {
     setState(() {
       _getIdentifiersResult = result.toString();
     });
-  }
-
-  // // UTIL
-  RichText getRichText(String label, String value) {
-    return new RichText(
-      text: new TextSpan(
-        // Note: Styles for TextSpans must be explicitly defined.
-        // Child text spans will inherit styles from parent
-        style: new TextStyle(
-          fontSize: 14.0,
-          color: Colors.black,
-        ),
-        children: <TextSpan>[
-          new TextSpan(
-              text: label, style: new TextStyle(fontWeight: FontWeight.bold)),
-          new TextSpan(text: value),
-        ],
-      ),
-    );
   }
 
   @override
